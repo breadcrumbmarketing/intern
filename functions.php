ab496126b295062f1b653cd338c4735ec5bbd498
hamyvosugh <online@breadcrumb.de>
2025-02-11 12:35:06 +0100

    qwe

--- functions.php
+++ functions.php
@@ -16,6 +16,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
 
+/**
+ * Load child theme scripts & styles.
+ *
+ * @return void
+ */
+function hello_elementor_child_scripts_styles() {
+
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		HELLO_ELEMENTOR_CHILD_VERSION
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+
 
 /**
  * Automate creation/update of Leo Custom Posts from database
